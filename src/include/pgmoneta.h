0f00e32801bec3b00dd0da34e0683f1149280faf
jesperpedersen <jesper.pedersen@redhat.com>
2021-08-05 07:44:33 -0400

    Next is 0.5.0

--- src/include/pgmoneta.h
+++ src/include/pgmoneta.h
@@ -41,7 +41,7 @@ extern "C" {
 #include <sys/types.h>
 #include <openssl/ssl.h>
 
-#define VERSION "0.4.0"
+#define VERSION "0.5.0"
 
 #define PGMONETA_HOMEPAGE "https://pgmoneta.github.io/"
 #define PGMONETA_ISSUES "https://github.com/pgmoneta/pgmoneta/issues"
