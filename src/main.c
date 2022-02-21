33d2b69d5552135cbec348d46aea3fb1a36e3799
Luca Ferrari <fluca1978@gmail.com>
2022-02-21 10:38:32 -0500

    [#42] Remove lock file management.
    Since the PID file is _always_ present, even when the user
    does not specify it in the configuration, it does not make any
    sense to keep track also of the lock file.
    
    This removes the usage of the `create_lockfile` and `remove_lockfile`
    functions.

--- src/main.c
+++ src/main.c
@@ -90,8 +90,6 @@ static void reload_configuration(void);
 static void init_receivewals(void);
 static int  create_pidfile(void);
 static void remove_pidfile(void);
-static int  create_lockfile(void);
-static void remove_lockfile(void);
 static void shutdown_ports(void);
 
 struct accept_io
@@ -491,11 +489,6 @@ main(int argc, char **argv)
       exit(1);
    }
 
-   if (create_lockfile())
-   {
-      exit(1);
-   }
-
    pgmoneta_set_proc_title(argc, argv, "main", NULL);
 
    /* Bind Unix Domain Socket */
@@ -663,7 +656,6 @@ main(int argc, char **argv)
    free(management_fds);
 
    remove_pidfile();
-   remove_lockfile();
 
    pgmoneta_stop_logging();
    pgmoneta_destroy_shared_memory(shmem, shmem_size);
@@ -1773,46 +1765,6 @@ remove_pidfile(void)
    {
       unlink(config->pidfile);
    }
-}
-
-static int
-create_lockfile(void)
-{
-   char* f = NULL;
-   int fd;
-
-   f = pgmoneta_append(f, "/tmp/pgmoneta.lock");
-
-   fd = open(f, O_WRONLY | O_CREAT | O_EXCL, 0644);
-   if (fd < 0)
-   {
-      printf("Could not create lock file '%s' due to %s\n", f, strerror(errno));
-      goto error;
-   }
-
-   close(fd);
-
-   free(f);
-
-   return 0;
-
-error:
-
-   free(f);
-
-   return 1;
-}
-
-static void
-remove_lockfile(void)
-{
-   char* f = NULL;
-
-   f = pgmoneta_append(f, "/tmp/pgmoneta.lock");
-
-   unlink(f);
-
-   free(f);
 }
 
 static void
