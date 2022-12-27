7f6aba21bd8cf2a0d8759cd71b838e91e7b1034e
jesperpedersen <jesper.pedersen@redhat.com>
2022-12-27 05:08:37 -0500

    [#103] Guard against invalid user configuration

--- src/libpgmoneta/configuration.c
+++ src/libpgmoneta/configuration.c
@@ -1129,6 +1129,11 @@ pgmoneta_read_users_configuration(void* shm, char* filename)
 
             ptr = strtok(NULL, ":");
 
+            if (ptr == NULL)
+            {
+               goto error;
+            }
+
             if (pgmoneta_base64_decode(ptr, strlen(ptr), &decoded, &decoded_length))
             {
                goto error;
@@ -1300,6 +1305,11 @@ pgmoneta_read_admins_configuration(void* shm, char* filename)
             username = ptr;
 
             ptr = strtok(NULL, ":");
+
+            if (ptr == NULL)
+            {
+               goto error;
+            }
 
             if (pgmoneta_base64_decode(ptr, strlen(ptr), &decoded, &decoded_length))
             {
