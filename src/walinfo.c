e1276048bdd0d22cd768717c6ad6dc42e7bf6fd1
jesperpedersen <jesper.pedersen@comcast.net>
2024-12-17 07:10:30 -0500

    [#435] walinfo: Support encrypted files

--- src/walinfo.c
+++ src/walinfo.c
@@ -100,6 +100,7 @@ main(int argc, char** argv)
    {
       static struct option long_options[] =
       {
+         {"configuration", required_argument, 0, 'c'},
          {"output", required_argument, 0, 'o'},
          {"format", required_argument, 0, 'F'},
          {"logfile", required_argument, 0, 'L'},
