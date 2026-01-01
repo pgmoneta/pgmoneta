65ac4177881e9f616c01844612f8ba2c258c9e2a
jesperpedersen <jesperpedersen.db@gmail.com>
2026-01-01 07:03:54 -0500

    Happy 2026 !

--- src/include/yaml_utils.h
+++ src/include/yaml_utils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The pgexporter community
+ * Copyright (C) 2026 The pgexporter community
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
